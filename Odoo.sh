7e7d6a0c6d461d56f3ed713d743df8ee3ab43491
waqasyousaf.pucit@gmail.com <waqasyousaf.pucit@gmail.com>
2022-05-11 19:40:48 +0500

    odoo_logo_tiny image replaced

--- Odoo.sh
+++ Odoo.sh
@@ -1,17 +1,19 @@
 echo "Debrant Starts at  $(date "+%H:%M:%S")"
-find /home/sohaib/Documents/WS/odoo-15.0/addons/ -type f -exec sed -i 's/Odoo/Midrarsolutions/g' {} \;
+find /odoo/odoo-server/addons/ -type f -exec sed -i 's/Odoo/Midrarsolutions/g' {} \;
 echo "Title is debrand at $(date "+%H:%M:%S")"
-find /home/sohaib/Documents/WS/odoo-15.0/addons/ -type f -exec sed -i 's/odoo.com/midrarsolutions.app/g' {} \;
+find /odoo/odoo-server/addons/ -type f -exec sed -i 's/odoo.com/midrarsolutions.app/g' {} \;
 echo "Website is debrand at $(date "+%H:%M:%S")"
-find /home/sohaib/Documents/WS/odoo-15.0/addons/ -type f -exec sed -i 's/Odoo.com/midrarsolutions.app/g' {} \;
+find /odoo/odoo-server/addons/ -type f -exec sed -i 's/Odoo.com/midrarsolutions.app/g' {} \;
 echo "Updating MidrarsolutionsEditor to OdooEditor at $(date "+%H:%M:%S")"
-find /home/sohaib/Documents/WS/odoo-15.0/addons/ -type f -exec sed -i 's/MidrarsolutionsEditor/OdooEditor/g' {} \;
+find /odoo/odoo-server/addons/ -type f -exec sed -i 's/MidrarsolutionsEditor/OdooEditor/g' {} \;
 echo "Complete debranding at $(date "+%H:%M:%S")"
 
-cp logo.png /home/sohaib/Documents/WS/odoo-15.0/addons/web/static/img
-cp logo2.png /home/sohaib/Documents/WS/odoo-15.0/addons/web/static/img
-cp favicon.ico /home/sohaib/Documents/WS/odoo-15.0/addons/web/static/img
-cp logo.png /home/sohaib/Documents/WS/odoo-15.0/odoo/addons/base/static/img/res_company_logo.png
-cp logo.png /home/sohaib/Documents/WS/odoo-15.0/odoo/addons/base/static/img/logo.png
-cp logo.png /home/sohaib/Documents/WS/odoo-15.0/odoo/addons/base/static/img/logo_white.png
-cp logo.png /home/sohaib/Documents/WS/odoo-15.0/odoo/addons/base/static/img/main_partner_image.png
+cp logo.png /odoo/odoo-server/addons/web/static/img
+cp odoo_logo_tiny.png /odoo/odoo-server/addons/web/static/img
+cp logo2.png /odoo/odoo-server/addons/web/static/img
+cp favicon.ico /odoo/odoo-server/addons/web/static/img
+cp logo.png /odoo/odoo-server/odoo/addons/base/static/img/res_company_logo.png
+cp logo.png /odoo/odoo-server/odoo/addons/base/static/img/logo.png
+cp logo.png /odoo/odoo-server/odoo/addons/base/static/img/logo_white.png
+cp logo.png /odoo/odoo-server/odoo/addons/base/static/img/main_partner_image.png
+
